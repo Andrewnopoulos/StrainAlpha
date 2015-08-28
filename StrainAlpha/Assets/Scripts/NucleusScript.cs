06d322816592e05d46e724bfe4202a42908b57bf
Andrew Giannopoulos <andrew.giannopoulos@gmail.com>
2015-08-28 11:54:13 +1000

    Added basic nucleus script

---
+++ StrainAlpha/Assets/Scripts/NucleusScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NucleusScript : MonoBehaviour {
+
+    private Chromosome myGenes;
+
+    public PlayerScript player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public NucleusScript(Chromosome initialGenes)
+    {
+        myGenes = initialGenes;
+    }
+}
