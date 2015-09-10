1f760883a140455d6cec7b71332f95a6cff5ec26
Iain <Dowling>
2015-09-10 10:32:26 +1000

    Added new canvas attached to the player

---
+++ StrainAlpha/Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void LateUpdate()
+    {
+        if (transform.rotation != Quaternion.Euler(90.0f, 0, 0))
+        {
+            transform.rotation = Quaternion.Euler(90.0f, 0, 0);
+        }
+    }
+}
