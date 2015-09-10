44885c16e42453263180464a0967c4c8a2297f3b
Iain <Dowling>
2015-09-10 11:11:47 +1000

    Current player UI expands on right thumb-stick press

--- StrainAlpha/Assets/Scripts/PlayerUI.cs
+++ StrainAlpha/Assets/Scripts/PlayerUI.cs
@@ -1,15 +1,50 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerUI : MonoBehaviour {
 
+    private bool statsExpanded = false;
+    private float expanding;
+
+    private Vector3[] targetPos;
+    private Image[] healthImages;
+
 	// Use this for initialization
 	void Start () {
 
+        healthImages = new Image[8];
+        targetPos = new Vector3[8];
+
+        healthImages[0] = gameObject.GetComponentsInChildren<Image>()[0];
+        healthImages[1] = gameObject.GetComponentsInChildren<Image>()[1];
+        healthImages[2] = gameObject.GetComponentsInChildren<Image>()[2];
+        healthImages[3] = gameObject.GetComponentsInChildren<Image>()[3];
+        healthImages[4] = gameObject.GetComponentsInChildren<Image>()[4];
+        healthImages[5] = gameObject.GetComponentsInChildren<Image>()[5];
+        healthImages[6] = gameObject.GetComponentsInChildren<Image>()[6];
+        healthImages[7] = gameObject.GetComponentsInChildren<Image>()[7];
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        expanding -= Time.deltaTime;
+
+        if (Input.GetButtonDown("RightStickPress"))
+        {
+            ShowStats();
+            statsExpanded = !statsExpanded;
+        }
+
+        if (expanding > 0)
+        {
+            for (int i = 0; i < 8; ++i)
+            {
+                Vector3 vel = Vector3.zero;
+                healthImages[i].transform.localPosition = Vector3.SmoothDamp(healthImages[i].transform.localPosition, targetPos[i], ref vel, 0.05f);
+            }
+        }
 
 	}
 
@@ -19,5 +54,25 @@ public class PlayerUI : MonoBehaviour {
         {
             transform.rotation = Quaternion.Euler(90.0f, 0, 0);
         }
+    }
+
+    void ShowStats()
+    {
+        if (!statsExpanded)
+        {
+            for (int i = 0; i < 8; ++i)
+            {
+                targetPos[i] = healthImages[i].transform.localPosition * 2;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < 8; ++i)
+            {
+                targetPos[i] = healthImages[i].transform.localPosition / 2;
+            }
+        }
+
+        expanding = 1.0f;
     }
 }
