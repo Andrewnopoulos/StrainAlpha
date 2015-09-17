f1ab68d2a3dee5ffaab7e5406f6842864f76c8f3
Iain <Dowling>
2015-09-17 14:32:03 +1000

    Added basic charge functionality

---
+++ StrainAlpha/Assets/Scripts/ChargeScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChargeScript : MonoBehaviour {
+
+    public float damage = 5.0f;
+
+    private Renderer renderer;
+    private Collider collider;
+
+    private bool active = false;
+
+    private int enemyLayer = 9;
+
+	// Use this for initialization
+	void Start () {
+
+        renderer = gameObject.GetComponent<Renderer>();
+        collider = gameObject.GetComponent<Collider>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer == enemyLayer && other.tag == "Enemy")
+        {
+            //deal damage to the enemy
+            BounceEnemy(other);
+            other.GetComponent<CellScript>().TakeDamage(damage);
+        }
+    }
+    public void SetActive(bool _active)
+    {
+        active = _active;
+        renderer.enabled = _active;
+        collider.enabled = _active;
+    }
+
+    public bool GetActive()
+    {
+        return active;
+    }
+
+    void BounceEnemy(Collider other)
+    {
+        Vector3 delta = gameObject.transform.position - other.transform.position;
+        delta = new Vector3(delta.x * 5.0f, 0, delta.z * 5.0f);
+        other.attachedRigidbody.AddForce(delta);
+    }
+}
