8823f317dda11687e1e2a511188cefc9825a70d9
Iain <Dowling>
2015-10-08 11:21:52 +1100

    Added basic boss attack types and prefab

---
+++ StrainAlpha/Assets/Scripts/BossScript.cs
@@ -0,0 +1,187 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum AttackType
+{
+    DORMANT = 0,
+    ATTACKRADIAL = 1,
+    ATTACKCIRCLE = 2,
+    ATTACKTARGET = 3,
+    SPAWNCELLS = 4,
+}
+
+public class BossScript : MonoBehaviour {
+
+    private AttackType attackType = AttackType.DORMANT;
+
+    private float health = 500.0f;
+    private float damage = 2.0f;
+    private float speed = 3.5f;
+
+    public int scoreWorth = 100000;
+
+    private float MaxSpeed = 5.0f;
+
+    private Vector3 target;
+    private Transform playerLocation;
+
+    public GameObject bulletPrefab;
+
+    private bool roaming = false;
+
+    private float dormantTime = 1.0f;
+    private float dormantCountdown = 1.0f;
+
+    private float shootCooldown;
+    private float shootRate;
+
+    private float attackLength = 5.0f;
+    private float attackCooldown = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+        playerLocation = GameObject.Find("Player").transform;
+        target = new Vector3(0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        switch (attackType)
+        {
+            case AttackType.DORMANT:
+                {
+                    Dormant();
+                    break;
+                }
+            case AttackType.ATTACKRADIAL:
+                {
+                    AttackRadial();
+                    break;
+                }
+            case AttackType.ATTACKCIRCLE:
+                {
+                    AttackCircle();
+                    break;
+                }
+            case AttackType.ATTACKTARGET:
+                {
+                    AttackTarget();
+                    break;
+                }
+            case AttackType.SPAWNCELLS:
+                {
+                    SpawnCells();
+                    break;
+                }
+        }
+
+	}
+
+    private void AttackRadial()
+    {
+        shootCooldown -= Time.deltaTime;
+        transform.Rotate(new Vector3(0, 1, 0), 360 * Time.deltaTime);
+        if (shootCooldown <= 0)
+        {
+            GameObject newBullet = (GameObject)Instantiate(bulletPrefab, transform.position, transform.rotation);
+            BulletScript script = newBullet.GetComponent<BulletScript>();
+            script.damage = damage;
+            script.speed = 15.0f;
+            script.SetAsEnemyBullet();
+            shootCooldown = shootRate;
+        }
+
+        attackCooldown -= Time.deltaTime;
+        if (attackCooldown <= 0)
+        {
+            attackType = AttackType.DORMANT;
+        }
+    }
+
+    private void AttackCircle()
+    {
+        shootCooldown -= Time.deltaTime;
+        if (shootCooldown <= 0)
+        {
+            for (int i = 0; i < 32; ++i)
+            {
+                transform.Rotate(new Vector3(0, 1, 0), (360 / 32) * i);
+                GameObject newBullet = (GameObject)Instantiate(bulletPrefab, transform.position, transform.rotation);
+                BulletScript script = newBullet.GetComponent<BulletScript>();
+                script.damage = damage;
+                script.speed = 15.0f;
+                script.SetAsEnemyBullet();
+            }
+            shootCooldown = shootRate;
+        }
+        attackCooldown -= Time.deltaTime;
+        if (attackCooldown <= 0)
+        {
+            attackType = AttackType.DORMANT;
+        }
+    }
+
+    private void AttackTarget()
+    {
+        shootCooldown -= Time.deltaTime;
+        transform.LookAt(playerLocation);
+        if (shootCooldown <= 0)
+        {
+            GameObject newBullet = (GameObject)Instantiate(bulletPrefab, transform.position, transform.rotation);
+            BulletScript script = newBullet.GetComponent<BulletScript>();
+            script.damage = damage;
+            script.speed = 15.0f;
+            script.SetAsEnemyBullet();
+            shootCooldown = shootRate;
+        }
+        attackCooldown -= Time.deltaTime;
+        if (attackCooldown <= 0)
+        {
+            attackType = AttackType.DORMANT;
+        }
+    }
+
+    private void Dormant()
+    {
+        dormantCountdown -= Time.deltaTime;
+        if (dormantCountdown <= 0)
+        {
+            int rand = (int)Random.Range(1.0f, 3.99f);
+            if (rand == 1)
+            {
+                attackType = AttackType.ATTACKRADIAL;
+                shootRate = 0.02f;
+                shootCooldown = 0.0f;
+                attackCooldown = attackLength;
+            }
+            else if (rand == 2)
+            {
+                attackType = AttackType.ATTACKCIRCLE;
+                shootRate = 0.5f;
+                shootCooldown = 0.0f;
+                attackCooldown = attackLength;
+            }
+            else if (rand == 3)
+            {
+                attackType = AttackType.ATTACKTARGET;
+                shootRate = 0.05f;
+                shootCooldown = 0.0f;
+                attackCooldown = attackLength;
+            }
+            else
+            {
+                attackType = AttackType.SPAWNCELLS;
+                shootRate = 0.3f;
+                shootCooldown = 0.0f;
+                attackCooldown = attackLength;
+            }
+            dormantCountdown = dormantTime;
+        }
+    }
+
+    private void SpawnCells()
+    {
+
+    }
+}
