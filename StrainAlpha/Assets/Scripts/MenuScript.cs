0dd9917aedb82a1597ff2ba523e04b43af29fa4b
Iain <Dowling>
2015-10-15 11:44:36 +1100

    Added code to handle the title screen and main menu

--- StrainAlpha/Assets/Scripts/MenuScript.cs
+++ StrainAlpha/Assets/Scripts/MenuScript.cs
@@ -3,13 +3,72 @@ using System.Collections;
 
 public class MenuScript : MonoBehaviour {
 
-	public void NextLevelButton(int index)
+    public GameObject TitleScreen;
+    public GameObject MainMenu;
+    public GameObject LevelSelect;
+    public GameObject LevelSelect_List;
+
+    public GameObject camera;
+    public Transform targetCameraPos;
+
+    public Transform[] cameraPositions;
+
+    void Start()
     {
-        Application.LoadLevel(index);
+        TitleScreen = GameObject.Find("Cnv_TitleScreen");
+        MainMenu = GameObject.Find("Cnv_MainMenu");
+        LevelSelect = GameObject.Find("Cnv_LevelSelect");
+        LevelSelect_List = GameObject.Find("Cnv_LevelSelect_List");
+
+        camera = GameObject.Find("Main Camera");
+
+        cameraPositions = new Transform[2];
+
+        cameraPositions[0] = GameObject.Find("CamPos_LevelSelect").GetComponent<Transform>();
+        cameraPositions[1] = GameObject.Find("CamPos_MainMenu").GetComponent<Transform>();
+
+        MainMenu.SetActive(false);
+        LevelSelect.SetActive(false);
+        LevelSelect_List.SetActive(false);
+
+        camera.transform.position = cameraPositions[1].position;
+        camera.transform.rotation = cameraPositions[1].rotation;
+        targetCameraPos = cameraPositions[1];
     }
 
-    public void NextLevelButton(string name)
+    void Update()
     {
-        Application.LoadLevel(name);
+        camera.transform.position = Vector3.Lerp(camera.transform.position, targetCameraPos.position, 0.07f);
+        camera.transform.rotation = Quaternion.Lerp(camera.transform.rotation, targetCameraPos.transform.rotation, 0.07f);
+    }
+
+    public void Load(string scene)
+    {
+        switch (scene)
+        {
+            case "TitleScreen":
+                targetCameraPos = cameraPositions[1];
+                TitleScreen.SetActive(true);
+                break;
+
+            case "MainMenu":
+                targetCameraPos = cameraPositions[1];
+                MainMenu.SetActive(true);
+                break;
+
+            case "LevelSelect":
+                targetCameraPos = cameraPositions[0];
+                LevelSelect.SetActive(true);
+                break;
+
+            case "LevelSelect_List":
+                targetCameraPos = cameraPositions[0];
+                LevelSelect_List.SetActive(true);
+                break;
+
+            default:
+                Debug.LogError("invalid level name encountered");
+                break;
+        }
     }
 }
