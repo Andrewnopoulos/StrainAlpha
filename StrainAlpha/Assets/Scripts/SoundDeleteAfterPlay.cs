4f26e98a78625b30bc24ad9409e7340ed5abc3ce
David Dam <damnation99@hotmail.com>
2015-10-22 11:32:57 +1100

    fixed menu materials and sounds

---
+++ StrainAlpha/Assets/Scripts/SoundDeleteAfterPlay.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundDeleteAfterPlay : MonoBehaviour {
+	AudioSource sound;
+	float timer;
+
+	// Use this for initialization
+	void Start () 
+	{
+		sound = GetComponent<AudioSource> ();
+		sound.Play ();
+		timer = sound.clip.length;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timer -= Time.deltaTime;
+		Debug.Log (timer);
+		if (timer <= -0.5f) 
+		{
+			Destroy(gameObject);
+		}
+	}
+}
